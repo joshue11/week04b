904df74e484ec1b1a23569747b13d76612386a8e
joshue11 <joshuemm2001@gmail.com>
2025-11-22 22:32:48 +0100

    Create Customer.cs

---
+++ OnlineOrdering/Customer.cs
@@ -0,0 +1,20 @@
+public class Customer
+{
+    private string _name;
+    private Address _address;
+
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
+
+    public bool LivesInUSA()
+    {
+        return _address.IsUSA();
+    }
+
+    public string GetName() => _name;
+
+    public Address GetAddress() => _address;
+}
